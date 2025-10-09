e53f164498d92fee598db68ffabaf4d94248ae2b
putra889-rgp <putraalfaro368@gmail.com>
2025-10-09 18:26:58 +0700

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -1,16 +1,16 @@
 import siakData from '../siak_clean_sample_1k.json';
 
 export default function handler(req, res) {
-  const nik = (req.query.nik || '').trim();
+  const nik = (req.query.NIK || '').trim();
 
-  console.log('Menerima NIK:', nik);
+  console.log('Menerima NIK:', NIK);
   console.log('Data JSON pertama:', siakData[0]);
 
-  if (!nik) {
+  if (!NIK) {
     return res.status(400).json({ error: 'NIK is required' });
   }
   
-  const result = siakData.find(item => item.nik === nik);
+  const result = siakData.find(item => item.NIK === NIK);
 
   if (result) {
     return res.status(200).json(result);
